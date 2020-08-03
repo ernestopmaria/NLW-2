1c4ae6cc62b54024b80159aaeb42a11ca27528b1
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-03 22:03:36 +0100

    prof list

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Routes from './routes';
+import './assets/styles/global.css';
+
+
 
 function App() {
   return (
- <h1>Hello World</h1>
+ <Routes />
   );
 }
 
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function TeacherForm(){
+    return(
+        <h1>TeacheForm</h1>
+    )
+}
+
+export default TeacherForm;
