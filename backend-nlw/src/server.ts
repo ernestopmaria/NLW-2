833e91a0da339ae43fa20877c9d71f3088599526
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-05 08:40:16 +0100

    configuração da rotas e o knex

--- backend-nlw/src/server.ts
+++ backend-nlw/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json())
+app.use(routes);
 
-app.get('/users' , (request, response) =>{
-  
-    return response.json({message: 'Hello world'})
-});
 
 app.listen(3333);
